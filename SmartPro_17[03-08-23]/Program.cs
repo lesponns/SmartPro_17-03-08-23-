6330695f5ade52477cc267f59500df4bd20bae18
lesponns <adamadakadam1903@gmail.com>
2023-08-03 18:46:34 +0300

    Add project files.

---
+++ SmartPro_17[03-08-23]/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_17_03_08_23_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double _user_number;
+
+            do
+            {
+                Console.Clear();
+                Console.Write("Positive Number: ");
+                _user_number = double.Parse(Console.ReadLine());
+                
+            }
+            while (_user_number < 0);
+
+            Console.Clear();
+            Console.WriteLine("Your Positive Number: " + _user_number);
+            Console.ReadKey();
+
+        }
+    }
+}
